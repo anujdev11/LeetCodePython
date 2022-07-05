218327ed18f13eb283a9b9c8479da7dcc583d047
Anuj Dev <37540714+anujdev11@users.noreply.github.com>
2022-07-05 20:36:31 -0300

    Time: 50 ms (91.04%), Space: 14.4 MB (66.61%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,15 +1,25 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-    
-        if len(t) < len(s):
+        if len(t) != len(s):
             return False
         
+        hashMap = {}
+        
         for i in s:
-            if i in t:
-                t = t.replace(i,"",1)
-           
-        if t == "":
-            return True
-        else:
-            return False
+            if i not in hashMap:
+                hashMap[i] = 1
+            else:
+                hashMap[i] +=1
+                
+        for i in t:
+            if i in hashMap:
+                hashMap[i] -=1
+            else:
+                return False
+            
+        for val in hashMap.values():
+            if val != 0:
+                return False
+        
+        return True
         
