9b9ab4afefbb7dd2321c46b3b1aa7711a33d655b
Anuj Dev <37540714+anujdev11@users.noreply.github.com>
2022-09-07 01:30:55 -0300

    Time: 696 ms (13.32%), Space: 16.5 MB (48.06%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -3,32 +3,53 @@ class Solution:
         
         
         
-        rows,cols = len(grid),len(grid[0])
+#         rows,cols = len(grid),len(grid[0])
         
-        noOfIslands = 0
+#         noOfIslands = 0
         
-        visited = set()
+#         visited = set()
         
-        def bfs(r,c):
-            queue = deque()
-            visited.add((r,c))
-            queue.append((r,c))
+#         def bfs(r,c):
+#             queue = deque()
+#             visited.add((r,c))
+#             queue.append((r,c))
             
-            while queue:
-                posibilities = [(1,0),(-1,0),(0,1),(0,-1)]
-                dr,dc = queue.popleft()
+#             while queue:
+#                 posibilities = [(1,0),(-1,0),(0,1),(0,-1)]
+#                 dr,dc = queue.popleft()
                 
-                for row,column in posibilities:
-                    r,c = dr+row,dc+column
+#                 for row,column in posibilities:
+#                     r,c = dr+row,dc+column
                         
-                    if(r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r,c) not in visited):
-                        visited.add((r,c))
-                        queue.append((r,c))
+#                     if(r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r,c) not in visited):
+#                         visited.add((r,c))
+#                         queue.append((r,c))
             
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == "1" and (r,c) not in visited: 
-                    bfs(r,c)
-                    noOfIslands+=1
+#         for r in range(rows):
+#             for c in range(cols):
+#                 if grid[r][c] == "1" and (r,c) not in visited: 
+#                     bfs(r,c)
+#                     noOfIslands+=1
                     
-        return noOfIslands
+#         return noOfIslands
+
+
+        if not grid:
+            return 0
+
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    count += 1
+        return count
+
+    def dfs(self, grid, i, j):
+        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
+            return
+        grid[i][j] = '#'
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i, j-1)
